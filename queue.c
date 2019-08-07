d464205610d1b4cf9a25a8ca0baebe0e651df4d8
Vlad Fomin <vfomin@cloudlinux.com>
2019-08-07 17:18:04 +0000

    added async queue swapper

--- queue.c
+++ queue.c
@@ -1,9 +1,14 @@
 #include "queue.h"
 
-static const char storage_dir[128] = "/root/mnt";
+static const char* storage_dir = "/root/mnt";
 
 static struct list_head queue;
 static atomic_t queue_size = ATOMIC_INIT(0);
+
+static struct task_struct* thread;
+static uint8_t thread_stopped;
+DECLARE_WAIT_QUEUE_HEAD(wait_queue);
+static atomic_t save_count = ATOMIC_INIT(0);
 
 static DEFINE_MUTEX(queue_rlock);
 static DEFINE_MUTEX(queue_wlock);
@@ -45,24 +50,6 @@ static int __save_data(struct data* d)
     d->on_disk = 1;
 
     return 0;
-}
-
-static ssize_t save_old_data(ssize_t nr)
-{
-    struct data* d;
-    ssize_t ret = 0;
-
-    if (nr <= 0)
-        return 0;
-
-    list_for_each_entry_reverse(d, &queue, next) {
-        __save_data(d);
-
-        if (--nr == 0)
-            break;
-    }
-
-    return ret - nr;
 }
 
 static ssize_t push_back(struct file* file, const char __user* buf, size_t count, loff_t* ppos)
@@ -132,10 +119,74 @@ static ssize_t pop_front(struct file* file, char __user* buf, size_t count, loff
     return ret;
 }
 
+static int save_old_data(ssize_t arg)
+{
+    struct data* d;
+    ssize_t nr = arg;
+
+    if (nr <= 0)
+        return 0;
+
+    list_for_each_entry_reverse(d, &queue, next) {
+        mutex_lock(&queue_rlock);
+        __save_data(d);
+        mutex_unlock(&queue_rlock);
+
+        if (atomic_dec_and_test(&save_count) == 0)
+            break;
+    }
+
+    return 0;
+}
+
+static int save_old_data_async(void* arg)
+{
+    struct data* d;
+    DECLARE_WAITQUEUE(wq, current);
+
+    printk(KERN_INFO "kthread state is wait sleep\n");
+    add_wait_queue(&wait_queue, &wq);
+
+    // enter in a loop waiting on the queue untill termination event
+    while (!thread_stopped)
+    {
+        printk(KERN_INFO "kthread state is TASK_INTERRUPTIBLE\n");
+        set_current_state(TASK_INTERRUPTIBLE);
+        // call the scheduler to release a CPU
+        schedule();
+        // the thread has been woken up, the thread is woken up in TASK_RUNNING state
+        printk(KERN_INFO "thread has been woken up in\n");
+
+        list_for_each_entry_reverse(d, &queue, next) {
+            mutex_lock(&queue_rlock);
+            __save_data(d);
+            mutex_unlock(&queue_rlock);
+
+            if (atomic_dec_and_test(&save_count))
+                break;
+        }
+    }
+
+    set_current_state(TASK_RUNNING);
+    remove_wait_queue(&wait_queue, &wq);
+    printk(KERN_INFO "kthread state is TASK_RUNNING\n");
+
+    do_exit(0);
+
+    return 0;
+}
+
 static long queue_ioctl(struct file* f, unsigned int ioctl, unsigned long count)
 {
-    if (ioctl == SAVE_SYNC)
+    pr_info("ioctl %u %lu\n", ioctl, count);
+
+    if (ioctl == 1 && !atomic_read(&save_count)) {
+        atomic_set(&save_count, count);
         save_old_data(count);
+    } else if (ioctl == 0) {
+        atomic_set(&save_count, count);
+        wake_up(&wait_queue);
+    }
 
     return 0;
 }
@@ -173,6 +224,8 @@ static int queue_init(void)
 
     pr_info("create queue %s\n", PROC_NAME);
     proc_create(PROC_NAME, 0, NULL, &queue_ops);
+    thread = kthread_create(save_old_data_async, NULL, "thread");
+    wake_up_process(thread);
 
     return 0;
 }
@@ -180,6 +233,12 @@ static int queue_init(void)
 static void queue_exit(void)
 {
     remove_proc_entry(PROC_NAME, NULL);
+
+    if (thread) {
+        thread_stopped = 1;
+        kthread_stop(thread);
+    }
+
     queue_cleanup();
     pr_info("unload queue %s\n", PROC_NAME);
 }
--- queue.h
+++ queue.h
@@ -8,6 +8,9 @@
 #include <linux/list.h>
 #include <linux/slab.h>
 #include <linux/uaccess.h>
+#include <linux/kthread.h>
+#include <linux/delay.h>
+#include <linux/wait.h>
 #include <linux/fs.h>
 #include <asm/atomic.h>
 
@@ -26,7 +29,8 @@ struct data {
 
 static int __load_data(struct data* d);
 static int __save_data(struct data* d);
-static ssize_t save_old_data(ssize_t nr);
+static int save_old_data(ssize_t arg);
+static int save_old_data_async(void* arg);
 static ssize_t push_back(struct file* file, const char __user* buf, size_t count, loff_t* ppos);
 static ssize_t pop_front(struct file* file, char __user* buf, size_t count, loff_t* ppos);
 static long queue_ioctl(struct file* f, unsigned int ioctl, unsigned long count);
